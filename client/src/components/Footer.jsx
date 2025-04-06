b07b3dd8eace2c5b5d42e383e87db57ad6f15686
BIRADAR VILOHITH PATIL <145685021+patilvilohith20@users.noreply.github.com>
2025-04-06 12:39:29 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
         <div className="sm:col-span-2">
           <h1 className="text-xl font-bold text-primary">
-            RideShare
+            Smart Carpooling
           </h1>
           <div className="mt-6 lg:max-w-sm">
             <p className="text-sm text-foreground">
@@ -23,7 +23,7 @@ const Footer = () => {
               aria-label="Our phone"
               title="Our phone"
             >
-              850-123-5021
+              7897897897
             </a>
           </div>
           <div className="flex">
@@ -33,7 +33,7 @@ const Footer = () => {
               aria-label="Our email"
               title="Our email"
             >
-              rideshare@gmail.com
+              complaints.smartcarpooling@gmail.com
             </a>
           </div>
           <div className="flex">
@@ -50,21 +50,21 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <span className="text-base font-bold text-foreground">Social</span>
+{/*           <span className="text-base font-bold text-foreground">Social</span>
           <div className="flex items-center mt-2 space-x-3">
             <a href="/"><Twitter /></a>
             <a href="/"><Instagram /></a>
-            <a href="/"><Facebook /></a>
+            <a href="/"><Facebook /></a> */}
           </div>
-          <p className="mt-4 text-sm">Follow us on social media</p>
+{/*           <p className="mt-4 text-sm">Follow us on social media</p> */}
         </div>
       </div>
       <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
-        <p className="text-sm">&copy; Copyright 2024 RideShare.com | All rights reserved.</p>
+        <p className="text-sm"></p>
         <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-          <li><a href="/" className="text-sm">F.A.Q</a></li>
-          <li><a href="/" className="text-sm">Privacy Policy</a></li>
-          <li><a href="/" className="text-sm">Terms &amp; Conditions</a></li>
+          <li><a href="/" className="text-sm"></a></li>
+          <li><a href="/" className="text-sm"></a></li>
+          <li><a href="/" className="text-sm"></a></li>
         </ul>
       </div>
     </footer>
