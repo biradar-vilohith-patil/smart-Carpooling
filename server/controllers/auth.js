6eced22fa1c09e95b32fa3d3ae47f2d13d4985c1
BIRADAR VILOHITH PATIL <145685021+patilvilohith20@users.noreply.github.com>
2025-04-09 01:10:14 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -49,7 +49,9 @@ export const login = async(req, res, next)=>{
       httpOnly: true,
       secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-      sameSite: 'strict' 
+      sameSite: "none",
+      secure: true
+
     };
 
     const { password, isAdmin, ...otherDetails } = user._doc;
