db77dab9f49188ccb4e1d93bc561be62d5830c35
BIRADAR VILOHITH PATIL <145685021+patilvilohith20@users.noreply.github.com>
2025-04-09 14:53:28 +0530

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -8,7 +8,6 @@ router.get("/", verifyAdmin, getAllUsers)
 router.get("/:id", verifyUser, getUser)
 router.patch("/:id", verifyUser, updateUser)
 router.delete("/:id", verifyUser, deleteUser)
-router.patch("/:id", verifyToken, updateUser);
 
 
 export default router
