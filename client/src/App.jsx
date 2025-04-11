82a3f84482ef0167c978dd4fcb0964b7e4fefd31
BIRADAR VILOHITH PATIL <patilvilohith20@gmail.com>
2025-04-12 00:53:15 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,31 +8,22 @@ import SearchPage from './pages/SearchPage'
 import Error from './pages/Error'
 import RideDetail from './pages/RideDetail'
 import Profile from './pages/Profile'
-import MapView from './components/MapView' // 🆕 Added
+ 
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<Error />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/offer-seat" element={<OfferSeat />} />
-        <Route path="/ride/:rideId" element={<RideDetail />} />
-        <Route path="/profile" element={<Profile />} />
-
-        {/* ✅ New Route for Testing Map */}
-        <Route path="/map" element={
-          <MapView 
-            origin={[28.6139, 77.2090]} 
-            destination={[28.4089, 77.3178]} 
-            currentLocation={[28.6200, 77.2200]} 
-          />
-        } />
-      </Routes>
-      <Footer />
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/*" element={<Error />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/offer-seat" element={<OfferSeat />} />
+      <Route path="/ride/:rideId" element={<RideDetail />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    <Footer />
     </>
   )
 }
