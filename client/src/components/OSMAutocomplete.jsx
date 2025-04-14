037a701691ece5deec57126aa60b8a55d887a84e
BIRADAR VILOHITH PATIL <patilvilohith20@gmail.com>
2025-04-15 00:37:35 +0530

    Update OSMAutocomplete.jsx

--- client/src/components/OSMAutocomplete.jsx
+++ client/src/components/OSMAutocomplete.jsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import {  useState } from "react";
+
+import {  useEffect } from "react";
 import axios from "axios";
 
 const OSMAutocomplete = ({ placeholder, onSelect, value }) => {
