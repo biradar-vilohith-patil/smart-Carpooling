d12d1c0900f4b69e95e2ff8dc92b66da9a6f0a4c
BIRADAR VILOHITH PATIL <patilvilohith20@gmail.com>
2025-04-26 20:29:47 +0530

    Update  rideController.js

---
+++ server/controllers/rideController.js
@@ -0,0 +1,89 @@
+// backend/controllers/rideController.js
+
+import Ride from "../models/Ride.js";
+import User from "../models/User.js";
+
+// Create a new Ride
+export const createRide = async (req, res, next) => {
+  try {
+    const newRide = new Ride({ ...req.body, creator: req.user.id });
+    await newRide.save();
+    await User.findByIdAndUpdate(req.user.id, { $push: { ridesCreated: newRide._id } });
+    res.status(201).json(newRide);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Search rides (Public route)
+export const searchRides = async (req, res, next) => {
+  try {
+    const { origin, destination, time } = req.query;
+
+    if (!origin || !destination || !time) {
+      return res.status(400).json({ message: "Origin, destination and time are required" });
+    }
+
+    const selectedTime = new Date(time);
+
+    const rides = await Ride.find({
+      "origin.place": { $regex: origin.trim(), $options: "i" },
+      "destination.place": { $regex: destination.trim(), $options: "i" },
+      startTime: { $gte: selectedTime }
+    })
+    .populate('creator', 'name profilePicture stars') 
+    .lean();
+
+    res.status(200).json({ rides });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get all rides
+export const getRides = async (req, res, next) => {
+  try {
+    const rides = await Ride.find().populate('creator', 'name profilePicture stars').lean();
+    res.status(200).json(rides);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get ride by ID
+export const getRide = async (req, res, next) => {
+  try {
+    const ride = await Ride.findById(req.params.id)
+      .populate('creator', 'name age profilePicture ridesCreated stars')
+      .lean();
+    if (!ride) return res.status(404).json({ message: "Ride not found" });
+    res.status(200).json(ride);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update ride
+export const updateRide = async (req, res, next) => {
+  try {
+    const updatedRide = await Ride.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedRide);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete ride
+export const deleteRide = async (req, res, next) => {
+  try {
+    await Ride.findByIdAndDelete(req.params.id);
+    await User.findByIdAndUpdate(req.user.id, { $pull: { ridesCreated: req.params.id } });
+    res.status(200).json({ message: "Ride deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
