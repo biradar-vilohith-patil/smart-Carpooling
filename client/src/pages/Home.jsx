26b05eae9d58815869d49192afa0cf913df02b91
BIRADAR VILOHITH PATIL <patilvilohith20@gmail.com>
2025-04-29 01:15:26 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import About from "@/sections/About"
 import Features from "@/sections/Features"
 import Hero from "@/sections/Hero"
 import Testimonial from "@/sections/Testimonial"
-import {Phonecall} from 'lucide-react';
+import {PhoneCall} from 'lucide-react';
 
 const Home = () => {
   return (
@@ -16,7 +16,7 @@ const Home = () => {
         className="fixed bottom-6 right-6 bg-red-600 hover:bg-red-700 text-white p-4 rounded-full shadow-lg z-50"
         title="Emergency SOS"
       >
-        <PhoneCall />
+        <PhoneCall size={24}/>
       </a>
      
     </>
